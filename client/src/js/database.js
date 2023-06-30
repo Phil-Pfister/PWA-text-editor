57d04fabc397e023d2c8de081b5cf08760313d5a
Phil-Pfister <salshouse@gmail.com>
2023-06-29 22:15:46 -0500

    manifest fix and database storage bug fix

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  try { 
+ 
     console.log('Post to the database');
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('jate', 'readwrite');
@@ -22,14 +22,12 @@ export const putDb = async (content) => {
     const request = store.add({ value: content });
     const result = await request;
     console.log('🚀 - data saved to the database', result);
-  } catch (error) {
-    console.error('putDb not implemented');
-  }
+  
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
+  
     console.log('GET all from the database');
     const jateDB = await openDB('jate', 1);
     const tx = jateDB.transaction('jate', 'readonly');
@@ -38,9 +36,7 @@ export const getDb = async () => {
     const result = await request;
     console.log('result.value', result);
     return result;
-      } catch (error) {
-    console.error('getDb not implemented');
-  }
+    
 }; 
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -21,32 +22,34 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Just another text editor',
+        
       }),
-     
+
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
         // TODO: Create a manifest.json: done! needs work
+        filename: 'manifest.json',
         fingerprints: false,
         inject: true,
         name: 'Just another text editor',
         short_name: 'J.A.T.E.',
         description: 'Text editor progressive web app',
         background_color: '#225ca3',
-        start_url: '/',
-        PublicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [
-      {
-        src: path.resolve('src/images/logo.png'),
-        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-        destination: path.join('assets', 'icons'),
-      },
-      
-    ]
-    
-      }), 
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
+          },
+
+        ]
+
+      }),
     ],
 
     module: {
@@ -57,7 +60,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
