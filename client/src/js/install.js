3d8efc24f1eb83350f5d20ff6f4005cfbd653a68
Phil-Pfister <salshouse@gmail.com>
2023-07-03 13:38:11 -0500

    bug fixes on app install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,24 +3,25 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPromt = event;
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
     
 });
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPromt;   
+    const promptEvent = window.deferredPrompt;   
     if (!promptEvent) {
         return
     }   
     promptEvent.prompt();
-    window.deferredPromt = null;
+    window.deferredPrompt = null;
     butInstall.style.visibility = 'hidden';    
         
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    window.defferedPrompt = null;
     butInstall.style.visibility = 'hidden';
     console.log('appinstalled', event);
 });
